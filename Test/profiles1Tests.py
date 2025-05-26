2c128d0df9e17a7707bc98d119fa196210a0e03f
Rickard Idengren <rickardi_90@hotmail.com>
2025-05-26 12:04:05 +0200

    Failing test

--- Test/profiles1Tests.py
+++ Test/profiles1Tests.py
@@ -10,13 +10,13 @@ class TestCSV(unittest.TestCase):
                     self.assertTrue(True)
                     return
                 
-#    def failed_test_annika_in_profiles1csv(self):
-#       with open('profiles1.csv', newline='') as f:
-#            reader = csv.DictReader(f)
-#            for row in reader:
-#                if row['Givenname'] == 'annika':
-#                    self.assertTrue(True)
-#                    return
+    def failed_test_annika_in_profiles1csv(self):
+       with open('profiles1.csv', newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row['Givenname'] == 'annika':
+                    self.assertTrue(True)
+                    return
                 
 if __name__ == '__main__':
     unittest.main()
